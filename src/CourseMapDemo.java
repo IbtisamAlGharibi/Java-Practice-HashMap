714ccda126e9e1cfd5d395af89270484f8c929e2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 10:45:36 +0400

    Create course HashMap, add elements and display it

---
+++ src/CourseMapDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class CourseMapDemo {
+
+    public static void main (String[] args){
+
+        HashMap<String,String> courseName = new HashMap<>();
+        courseName.put("ab001", "Java");
+        courseName.put("ab002","python");
+        courseName.put("ab003", "Database");
+        courseName.put("ab004", "NW");
+        courseName.put("ab005", "C++");
+        System.out.println(courseName);
+
+
+}}
+
