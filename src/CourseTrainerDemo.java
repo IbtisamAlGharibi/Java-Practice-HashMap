d0c27c7f707fe0095f2093f6e6f4d680e5cc5c94
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 10:24:46 +0400

    Create course HashMap, add elements and display it

---
+++ src/CourseTrainerDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class CourseTrainerDemo {
+
+    public static void main (String[] args){
+
+        HashMap<String,String> courseName = new HashMap<>();
+        courseName.put("IT", "Ibtisam");
+        courseName.put("NW","Muna");
+        courseName.put("SW", "Nora");
+        courseName.put("HR", "Sara");
+        courseName.put("DM", "Anfal");
+        System.out.println(courseName);
+
+
+    }
+}
