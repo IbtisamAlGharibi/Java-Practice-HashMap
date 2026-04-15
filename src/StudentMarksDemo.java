dafa4360fec523842d835163de7abf2125d9cf32
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 09:30:35 +0400

    Create the HashMap,Add elements and display it

---
+++ src/StudentMarksDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class StudentMarksDemo {
+
+    public static void main(String[] args){
+
+        HashMap<Integer,Integer> studentMarks = new HashMap<>();
+        studentMarks.put(001, 99);
+        studentMarks.put(002, 95);
+        studentMarks.put(003, 80);
+        studentMarks.put(004, 85);
+        studentMarks.put(005, 90);
+        System.out.println(studentMarks);
+
+
+    }
+}
