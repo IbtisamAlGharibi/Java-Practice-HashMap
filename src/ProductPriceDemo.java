c0877a82574e6b7652ff221a7223e4b88ef50637
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 09:47:59 +0400

    creat the HashMap , add elements and display

---
+++ src/ProductPriceDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class ProductPriceDemo {
+
+    public static void main(String[] args){
+        HashMap<String,Double>  productNames = new HashMap<String, Double>();
+        productNames.put("prod1", 3.5);
+        productNames.put("prod2",4.2);
+        productNames.put("prod3", 5.5);
+        productNames.put("prod4", 2.5);
+        productNames.put("prod5", 6.4);
+
+        System.out.println(productNames);
+        Double first = productNames.get("prod2");
+        System.out.println(first);
+
+
+
+
+
+    }
+}
