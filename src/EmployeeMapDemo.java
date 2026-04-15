536384504d471d3a414b2ac653b7beb4935b597d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 10:36:25 +0400

    Create employee names HashMap, add elements and display it

---
+++ src/EmployeeMapDemo.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class EmployeeMapDemo {
+
+    public static void main(String[] args){
+        HashMap<Integer,String> employeeNames = new HashMap<>();
+        employeeNames.put(001,"Ibtisam");
+        employeeNames.put(002,"Anfal");
+        employeeNames.put(003,"Sara");
+        employeeNames.put(004,"Maather");
+        employeeNames.put(005, "Muna");
+        System.out.println(employeeNames);
+
+}}
+
+
