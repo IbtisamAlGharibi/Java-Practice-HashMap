5d73d73200d2957d4979655732ee387c8e59f32f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 10:14:14 +0400

    create the book hashmap, add elements to it and display

---
+++ src/BookAuthorDemo.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class BookAuthorDemo {
+    public static void main(String [] args){
+
+        HashMap<String, String> books = new HashMap<>();
+        books.put("Harry Potter", "Harry");
+        books.put("The Secret Garden","Jone");
+        books.put("Matilda", "Marry");
+        books.put("The giver", "Jerry");
+        books.put("The Golden Rose", "Rosaline");
+        System.out.println(books);
+
+
+
+    }
+}
